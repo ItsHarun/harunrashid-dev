48ee8f60c1f6e457116827d0da478e8753910900
ItsHarun <harunrashidvnr8@gmail.com>
2026-02-15 15:32:10 +0530

    refactor: decompose monolithic storage.ts into dedicated service modules and update routes to use them.

---
+++ server/services/index.ts
@@ -0,0 +1,5 @@
+// Export all services from a central location
+export * from './posts.service';
+export * from './beliefs.service';
+export * from './now.service';
+export * from './messages.service';
